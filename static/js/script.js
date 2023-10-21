e1448718f43479b9f64d018da73252d8c5e1bcb1
mahdi <madavid022@student.wethinkcode.co.za>
2023-10-21 21:45:14 +0200

    just added extra padding

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,25 @@
-function sendOrder() {
-    var btn = document.getElementById("order-btn");
-    var input1 = document.getElementById("input1");
-    var input2 = document.getElementById("input2");
-    
-    if (input1.value === "" || input2.value === "") {
-      alert("Please fill in both boxes.");
-      return;
-    }
-    
-    btn.classList.add("order-sent");
-    btn.innerHTML = '<i class="fas fa-check"></i> Order Sent';
-  }
-  
+document.addEventListener("DOMContentLoaded", function () {
+    const rows = document.querySelectorAll("tbody tr");
+    const deleteButtons = document.querySelectorAll(".delete-button");
+
+    rows.forEach((row, index) => {
+        row.addEventListener("click", () => {
+            // Hide all delete buttons
+            deleteButtons.forEach(button => button.style.display = "none");
+
+            // Show the delete button for the clicked row
+            const deleteButton = row.querySelector(".delete-button");
+            deleteButton.style.display = "block";
+        });
+
+        // Prevent the button click from triggering the row click
+        const deleteButton = row.querySelector(".delete-button");
+        deleteButton.addEventListener("click", (event) => {
+            event.stopPropagation();
+            // Implement your delete logic here
+            // You can access the data for the row using the 'index'
+            // For example, orders[index] represents the data for the clicked row
+            console.log("Delete clicked for row", index);
+        });
+    });
+});
